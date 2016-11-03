4a14bf78ef6ef333a9370269f4b9f95d2c37deb2
Khadijah <khadijah.s@opensooq.com>
2016-11-03 12:49:17 +0200

    Change Lib

---
+++ HttpClient.php
@@ -0,0 +1,92 @@
+<?php
+
+class HttpClient {
+    public static $commonOptions=[
+        CURLOPT_USERAGENT      => 'OpenSooqClient 1.0',
+        CURLOPT_HTTPHEADER     => ['Expect:'],
+        CURLOPT_TIMEOUT        => 10,
+        CURLOPT_CONNECTTIMEOUT => 30,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_BINARYTRANSFER => true,
+        CURLOPT_HEADER         => false,
+        CURLOPT_FRESH_CONNECT  => false,
+        CURLOPT_FORBID_REUSE   => false,
+    ];
+    public static function post($url,$postParams,$getParams=[], $headers=[], $options=[]) {
+        return static::request('POST',$url,$getParams,$postParams, $headers, $options);
+
+    }
+    public static function get($url, $getParams=[], $headers=[],$options=[]) {
+        return static::request('GET',$url,$getParams,null,$headers, $options);
+
+    }
+
+    public static function jsonPost($url,$postParams,$getParams=[], $headers=[], $options=[]) {
+        $headers['Content-Type']= 'application/json';
+        static::post($url,$postParams,$getParams, $headers, $options);
+    }
+    public static function request($verb,$url,$getParams=[],$body=null, $headers=[], $options=[]) {
+        $opt     = $options + static::$commonOptions;
+        // Check if the header is Application Json
+        $isJson = false;
+
+        if(isset($headers['Content-Type'])) {
+            $isJson = true;
+        }
+        $headers = static::_formatHeaders($headers);
+        $opt[CURLOPT_HTTPHEADER]   = $headers + $opt[CURLOPT_HTTPHEADER];
+        $opt[CURLOPT_CUSTOMREQUEST] = $verb;
+        $opt[CURLOPT_URL]           = static::_urlWithGetParams($url,$getParams);
+
+        if ($body===null) {
+            $opt[CURLOPT_NOBODY]=true;
+        } elseif (is_string($body)) {
+            $opt[CURLOPT_POSTFIELDS]=$body;
+        } elseif (!$isJson) {
+            $opt[CURLOPT_POSTFIELDS]=json_encode($body);
+        } else {
+            $opt[CURLOPT_POSTFIELDS]=http_build_query($body);
+
+        }
+        $handler = curl_init();
+        curl_setopt_array($handler, $opt);
+        if (! $response = curl_exec($handler)) {
+            return [
+                [
+                    'code'=>'',
+                    'Content-Type'=>'',
+                    'curl-Error'=>curl_error($handler)
+                ],
+                ''
+            ];
+        }
+        $info=[
+            'code'         => curl_getinfo($handler, CURLINFO_HTTP_CODE),
+            'Content-Type' => curl_getinfo($handler, CURLINFO_CONTENT_TYPE),
+            'curl-Error'   => null
+        ];
+        return ['info'=>$info, 'response'=>$response];
+    }
+    private static function _urlWithGetParams($url,$getParams=null) {
+        if ($getParams!==null) {
+            $url .= (strpos($url, '?') === false ? '?' : '&')
+                . (is_string($getParams) ? $getParams : http_build_query($getParams));
+        }
+        return $url;
+    }
+    /**
+     * return curl headers on curl format
+     * @return array
+     */
+    private static function _formatHeaders($headers)
+    {
+        $result= [];
+        foreach ($headers as $name => $value) {
+            if (is_null($value)) {
+                $value='';
+            }
+            array_push($result, "$name: $value");
+        }
+        return $result;
+    }
+}
